aa9e7d02b560431511f785fb13d3eb530841dba0
Manuel Sande <mnlsande22@gmail.com>
2019-12-25 03:41:19 -0500

    create tests for pojos

--- src/test/java/models/TaskTest.java
+++ src/test/java/models/TaskTest.java
@@ -2,7 +2,8 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
-
+import org.junit.Test;
+import java.time.LocalDateTime;
 import static org.junit.Assert.*;
 
 public class TaskTest {
@@ -14,4 +15,33 @@ public class TaskTest {
     @After
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void NewTaskObjectGetsCorrectlyInstantiated_true() throws Exception {
+        Task task = setupNewTask();
+        assertEquals(true, task instanceof Task);
+    }
+
+    @Test
+    public void TaskInstantiatesWithDescription_true() throws Exception {
+        Task task = setupNewTask();
+        assertEquals("Wash Dishes", task.getDescription());
+    }
+
+    @Test
+    public void isCompletedPropertyIsFalseAtInstantiation() throws Exception {
+        Task task = setupNewTask();
+        assertEquals(false, task.getCompleted());
+    }
+
+    @Test
+    public void getCreatedAtInstantiatesWithCurrentTimeToday() throws Exception {
+        Task task = setupNewTask();
+        assertEquals(LocalDateTime.now().getDayOfWeek(), task.getCreatedAt().getDayOfWeek());
+    }
+
+    public Task setupNewTask() {
+        return new Task("Wash Dishes");
+    }
+
 }
