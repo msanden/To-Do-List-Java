2579abbc24f40783ba8eeccf60348b0dfd7c382a
Manuel Sande <mnlsande22@gmail.com>
2019-12-25 03:56:24 -0500

    Test the DAO interface

--- src/test/java/dao/Sql2oTaskDaoTest.java
+++ src/test/java/dao/Sql2oTaskDaoTest.java
@@ -1,4 +1,27 @@
 package dao;
 
+import models.Task;
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class Sql2oTaskDaoTest {
+
+    private Sql2oTaskDao taskDao;
+    private Connection conn;
+
+    /* our @Before annotation runs before every test, and our @After annotation runs after every test. */
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        taskDao = new Sql2oTaskDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+
 }
