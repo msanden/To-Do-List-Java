c0da1ae01f8d4f2fc85f604318f20957e8c6c283
Manuel Sande <mnlsande22@gmail.com>
2020-02-02 14:54:01 -0500

    test category methods

--- src/test/java/models/CategoryTest.java
+++ src/test/java/models/CategoryTest.java
@@ -1,7 +1,24 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CategoryTest {
 
+    @Test
+    public void NewCategoryObjectGetsCorrectlyInstantiated_true() throws Exception {
+        Category category = setUpNewCategory();
+        assertEquals(true, category instanceof Category);
+    }
+
+    @Test
+    public void CategoryInstantiatesWithDescription_true() throws Exception {
+        Category category = setUpNewCategory();
+        assertEquals("Work", category.getName());
+    }
+
+    public Category setUpNewCategory() {
+        return new Category("Work");
+    }
 }
