5a03627ffd211bc2c848e7b1c46f41e09bda627e
Manuel Sande <mnlsande22@gmail.com>
2020-02-02 14:47:10 -0500

    create categories table

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -3,5 +3,11 @@ SET MODE PostgreSQL;
 CREATE TABLE IF NOT EXISTS tasks (
     id int PRIMARY KEY auto_increment,
     description VARCHAR,
-    completed BOOLEAN
+    completed BOOLEAN,
+    categoryId INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id int PRIMARY KEY auto_increment,
+    name VARCHAR
 );
