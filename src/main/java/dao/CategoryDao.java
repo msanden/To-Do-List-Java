ec98b22b41ea2cad58c83a2c7b793253a981468b
Manuel Sande <mnlsande22@gmail.com>
2020-02-02 14:39:48 -0500

    import dependencies/models

--- src/main/java/dao/CategoryDao.java
+++ src/main/java/dao/CategoryDao.java
@@ -1,4 +1,22 @@
 package dao;
 
+import models.Category;
+import models.Task;
+import java.util.List;
+
 public interface CategoryDao {
+
+    void add(Category category);
+
+    Category findById(int id);
+
+    List<Category> getAll();
+
+    List<Task> getAllTasksByCategory(int categoryId);
+
+    void update(int id, String name);
+
+    void deleteById(int id);
+    void clearAllCategories();
+
 }
